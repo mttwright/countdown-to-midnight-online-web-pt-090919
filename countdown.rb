a60dded3f1c3176365997e6b4b1c55006aec936a
virtual-frame-7789 <mttwright@gmail.com>
2019-09-14 15:17:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number = 10)
+  
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+
+  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
+
+
 end
+
+countdown
